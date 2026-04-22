c4bbfb8d3fcae8d4646b2a17ef2f7b6fd42bd3af
David Ruvalcaba <166982620+LDGnx-dev@users.noreply.github.com>
2026-04-22 02:39:41 -0700

    Add random seed generation based on current time

--- data/patterns.py
+++ data/patterns.py
@@ -1,20 +1,37 @@
 import random
+import math
+from datetime import datetime
 
 def get_preset_pattern(pattern_name, default_w, default_h):
     """Devuelve (semillas, ancho, alto) según el patrón elegido"""
+
+################ Glider preset ################
     
     if pattern_name == 'glider':
         return [(1,0), (2,1), (0,2), (1,2), (2,2)], default_w, default_h
-        
+
+################ Random Seed preset ################
+    
     elif pattern_name == 'random':
+        now = datetime.now()
+        minute_block = math.floor(now.minute / 5) * 5
+        time_seed = f"{now.strftime('%Y-%m-%d-%H')}-{minute_block}"
+        random.seed(time_seed)
+
         num_cells = random.randint(70, 100)
         seeds = [(random.randint(0, default_w-1), random.randint(0, default_h-1)) for _ in range(num_cells)]
-        return seeds, default_w, default_h
+        random.seed() 
         
+        return seeds, default_w, default_h
+
+################ R-Pentomino preset ################
+    
     elif pattern_name == 'r-pentomino':
         cx, cy = 26, 3
         return [(cx, cy-1), (cx+1, cy-1), (cx-1, cy), (cx, cy), (cx, cy+1)], default_w, default_h
-        
+
+################ Glider Factory preset ################
+    
     elif pattern_name == 'gun':
         w, h = 40, 25
         gun = [
@@ -24,5 +41,6 @@ def get_preset_pattern(pattern_name, default_w, default_h):
             (22, 6), (24, 6), (10, 7), (16, 7), (24, 7), (11, 8), (15, 8), (12, 9), (13, 9)
         ]
         return gun, w, h
-        
-    return None, default_w, default_h
+
+    
+return None, default_w, default_h
